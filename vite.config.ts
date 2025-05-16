e15009de560dd0c53b0cca50a9098e90361f18dc
DulceChidembo1 <dulcechidembue@gmail.com>
2025-05-16 16:25:57 +0200

    fix: resolve conflits

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-<<<<<<< HEAD
+
+export default defineConfig({
 
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-=======
 
-// https://vite.dev/config/
-export default defineConfig({
-<<<<<<< HEAD
-  plugins: [react(), tailwindcss(),],
-=======
-  plugins: [react(),   tailwindcss(),],
->>>>>>> mec
->>>>>>> 08570afe79038e9f08abbbd1be8124d03a41992e
+  plugins: [react(), tailwindcss(),]
 })
