399f872497dfad82243c7d214510100fda516dc8
isaneyde <issitoe941@gmail.com>
2025-05-19 09:30:31 +0200

    Fix:Fix navigation on pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-<<<<<<< HEAD
 
-export default defineConfig({
-
-
-// https://vite.dev/config/
-
-  plugins: [react(), tailwindcss(),]
-=======
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
->>>>>>> db453fe8f1184b6d849c4cda36f14836e8ec9b07
+
 })
