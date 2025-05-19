5873a0807c811ba402a5902c714fa8b3ddf08a9c
MequeChiziane <mequechiziane@gmail.com>
2025-05-19 11:05:43 +0200

    Up date buttons and install fonts

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -3,15 +3,13 @@ export const Home = () => {
     return (
         <body className="py-10 bg-black text-black font-bold">
             <div>
-                <h1 className="text-2xl text-center text-white"> Ola, bem vindo(a)</h1>
-                <img className="" alt="Logotipo" src="public/img/spoti_logo.png" />
+                <h1 className=" font-YuseiMagic text-[32px] m-15 text-2xl text-center text-white"> Ola, bem vindo(a)</h1>
+                <img className="" alt="Logotipo" src="/img/spoti_logo.png" />
 
                 <section>
-                    <NavLink className="bg-amber-400 m-10 p-4 w-2xs rounded-full shadow-md" to="/Login">
-Login
+                    <NavLink className="flex justify-around bg-amber-400 m-20 p-4 w-2xs rounded-full shadow-md" to="/Login">Login
                     </NavLink>
-                         <NavLink className="bg-amber-400 m-10 p-4 w-2xs rounded-full shadow-md" to="Sign">
-                        Criar Conta
+                     <NavLink className="flex justify-around bg-amber-400 m-20 p-4 w-2xs rounded-full shadow-md" to="Sign">Criar Conta
                     </NavLink>
                    
                 </section>
