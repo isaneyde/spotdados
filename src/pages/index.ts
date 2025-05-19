63f142ba9823b675bcfa2b037908ebeb7d20a4a0
isaneyde <issitoe941@gmail.com>
2025-05-19 09:16:22 +0200

    fix:fix the routes

---
+++ src/pages/index.ts
@@ -0,0 +1,8 @@
+import {Home} from "./home";
+import { Sign } from "./sign-in";
+import { Login } from "./login";
+import { UsersList } from "./list-users";
+import { ListMusic} from "./list-music";
+
+export {Home,Sign,Login,UsersList,ListMusic};
+
