704d7134a4f4dc214fe582f6698cdbb99624eb8a
DulceChidembo1 <dulcechidembue@gmail.com>
2025-05-19 15:04:06 +0200

    feat: create struture login

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,19 +1,20 @@
-import { EyeSlashIcon, } from "@phosphor-icons/react";
+import { EnvelopeSimpleIcon, EyeSlashIcon, } from "@phosphor-icons/react";
 import { NavLink } from "react-router-dom";
 const style =
-  "border-1 rounded-3xl py-0 px-4 text-center min-w-75 max-w-100 h-10 m-10";
+  "border-1 rounded-3xl py-0 px-4 bg-white text-center min-w-75 max-w-100 h-10 m-10";
 
 export const Login = () => {
   return (
     <>
-    <body>
+    <body className="py-20 bg-[(public/img/background.png)] bg-black">
       <div>
-      <h1 className="text-black text-center px-4 py-2 font-bold mb-10 mt-16">
+      <h1 className="text-white text-center px-4 py-2 font-bold mb-10 mt-16">
         Ola, Bem Vindo(a)
       </h1>
       <div>
         <img className="mb-20" src="public/img/spoti_logo.png" alt="" />
       </div>
+      <h1 className="text-amber-400 font-extrabold text-center">Login</h1>
 
       <form
         action=""
@@ -31,19 +32,24 @@ export const Login = () => {
           placeholder="Password"
           required
         />
+        <div className="remember=forget flex flex-row justify-around">
+          <label className="text-white  font-light"><input type="checkbox" />Remember me</label>
+          <a href="#" className="text-white text-center font-light ">Forgot password?</a>
+        </div>
         <NavLink
-          className="p-3 rounded-3xl bg-amber-400 font:bold text-center w-75 m-10"
+          className="p-3 rounded-3xl bg-amber-400 font:bold text-center align-items-w-75 m-10"
           to="/Sign"
           type="submit"
         >
           Entrar
         </NavLink>
       </form>
+      <EnvelopeSimpleIcon size={12} weight="regular" color="black" className="ms-90 -mt-108"/>
       
       <EyeSlashIcon
         size={12}
         weight="regular"
-        className="ms-90 -mt-56"
+        className="ms-90 mt-35"
         color="black"
       />
     </div>
