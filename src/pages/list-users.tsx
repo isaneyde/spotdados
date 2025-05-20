b92d09cf5a9a5d45dcadfa3f5770f58c4cb59183
OlgaBernece258 <olgabernecepintomacamo@gmail.com>
2025-05-20 16:34:51 +0200

    upadate of the userlist page

--- src/pages/list-users.tsx
+++ src/pages/list-users.tsx
@@ -2,7 +2,7 @@
 import { useState } from 'react';
 import { Header } from '../components/header';
 import { users } from '../data/data';
-import {UserSquareIcon } from '@phosphor-icons/react';
+import {UserIcon } from '@phosphor-icons/react';
 import { Footer } from '../components/footer';
 
 export const UsersList = () => {
@@ -17,16 +17,17 @@ export const UsersList = () => {
     setDisplayedUsers(filtered);
   };
   const present = displayedUsers.map(user => (
-            <li  key={user.id} className="mb-8 bg-amber-400 px-6 py-6 border-5 rounded-3xl solid #ccc text-2xl border-amber-400 opacity-75 ">
-              <UserSquareIcon size={40} className='inline'/>{user.name }
+            <li  key={user.id} className="mb-8 bg-black px-6 py-6 border-4  rounded-3xl solid #ccc text-2xl border-white opacity-100 ">
+              <UserIcon size={40}  className='inline fill-amber-50'/>{user.name }
+              
             </li>
           ))
 
   return (
     <>
-      <Header title="Usuários" search={handleSearch} entrySearch={searchQuery} />
-      <main className='min-h-screen py-8 px-6 bg-[url(./img/bg-3.png)]  bg-black text-black font-bold  '>
-        <h2 className='text-3xl pb-5'>Usuários</h2>
+      <Header title="Usuários"  search={handleSearch} entrySearch={searchQuery} />
+      <main className='min-h-screen py-8 px-6 bg-[url(./img/bg-4.png)]  bg-black   text-amber-500 text font-bold  '>
+        <h2 className='text-3xl pb-5 text-amber-50'>Usuários</h2>
         <ul>
         {present}
         </ul>
