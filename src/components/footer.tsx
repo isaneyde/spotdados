b077949a5d930fbb51306a6972d4acd574e07713
isaneyde <issitoe941@gmail.com>
2025-05-20 22:17:34 +0200

    fix: fix the footer routes

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,12 +5,12 @@ import { NavLink } from "react-router-dom"
  export const Footer = () => {
   return (
     <footer className=" bg-black flex justify-between items-center p-4 ">
-      <NavLink to="../login.tsx">
+      <NavLink to="/login">
         <CaretLeftIcon className="h-6 w-6 text-white" />
       </NavLink>
       <HouseIcon className="h-6 w-6 text-white w-fill" />
 
-      <NavLink to="../pages/list-music.tsx">
+      <NavLink to="/list-music">
         <CaretRightIcon className="h-6 w-6 text-white" />
       </NavLink>
     </footer>
